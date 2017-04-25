3bdde2d82a390a3c3e4de32ef2d7027980afa702
Edem Kumodzi <edem@kumodzi.com>
2017-04-25 10:50:23 +0000

    keep codecov while codeclimate prepares reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
+require 'simplecov'
 SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
