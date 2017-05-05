5006b16777762ad6348991791b2ee3429fd70704
Bubunyo Nyavor <bubu@afradio.co>
2017-05-05 23:56:54 +0000

    pause on candidate to generate constituency model

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,9 @@
 class Candidate < ApplicationRecord
+  belongs_to :political_party, required: false
+  belongs_to :election
+
+  enum sex: { female: 'fem', male: 'mal' }
+
+  # validates_presence_of :constituency, :if => Proc.new { |c| !c.is_presidential_candidate }
+
 end
