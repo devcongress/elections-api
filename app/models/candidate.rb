277365213736cfe58d9c21a6895d07aae2f44d75
Bubunyo Nyavor <bubu@afradio.co>
2017-05-06 01:00:26 +0000

    work on candidate model

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,9 +1,31 @@
 class Candidate < ApplicationRecord
   belongs_to :political_party, required: false
   belongs_to :election
+  # belongs_to :constituency
 
   enum sex: { female: 'fem', male: 'mal' }
 
-  # validates_presence_of :constituency, :if => Proc.new { |c| !c.is_presidential_candidate }
+  validate :is_older_than18
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :date_of_birth, presence: true
 
+  with_options if: :is_presidential_candidate? do |candidate|
+    candidate.validates :constituency, :presence
+    candidate.validates :is_older_than_40 
+  end
+
+  private
+
+  def is_older_than_18
+    if (date_of_birth + 18.years) < Date.today
+      errors.add(:date_of_birth, "candidate is not old enough")
+    end
+  end
+
+  def is_older_than_40
+    if (date_of_birth + 40.years) < Date.today
+      errors.add(:date_of_birth, "presidential candiate is not old enough")
+    end
+  end
 end
--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @prez_candidate = candidates(:presidential_candidate)
+    @parli_candidate = candidates(:parliamentary_candidate)
+  end
+
+  test "should save presidential candidate who is older than 40" do
+    assert_not @election.save
+  end
+
+  test "should not save presidential candidate who is not 40 or older" do
+  end
+
+  test "should not save parlimentary  candidate older than 18" do
+  end
+
+  test "if not presidential candidate,  should be belong to constituency" do
+    #TODO(bubunyo) pull constituency model from petersowah and do test
+  end
+
 end
