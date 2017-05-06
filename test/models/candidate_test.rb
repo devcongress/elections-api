3a0313332dec42169ee56b29f9664bc435268c50
Bubunyo Nyavor <bubu@afradio.co>
2017-05-06 12:11:08 +0000

    travis complaints solved

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -10,27 +10,30 @@ class CandidateTest < ActiveSupport::TestCase
   end
 
   test "should not save candidate who is younger than 18 during election" do
-    @parli_candidate.date_of_birth =  @parli_candidate.date_of_birth.change(year: (@election.year - 17))
+    @parli_candidate.date_of_birth =
+      @parli_candidate.date_of_birth.change(year: (@election.year - 17))
     assert_not @parli_candidate.save
   end
 
   test "should not save presidential candidate who is not 40 or older" do
-    @prez_candidate.date_of_birth =  @prez_candidate.date_of_birth.change(year: (@election.year - 39))
+    @prez_candidate.date_of_birth =
+      @prez_candidate.date_of_birth.change(year: (@election.year - 39))
     assert_not @prez_candidate.save
   end
 
   test "should save parlimentary  candidate older than 18" do
-    @parli_candidate.date_of_birth =  @parli_candidate.date_of_birth.change(year: (@election.year - 19))
+    @parli_candidate.date_of_birth =
+      @parli_candidate.date_of_birth.change(year: (@election.year - 19))
     assert @parli_candidate.save
   end
 
   test "should save presidential candidate older than 40" do
-    @prez_candidate.date_of_birth =  @prez_candidate.date_of_birth.change(year: (@election.year - 49))
+    @prez_candidate.date_of_birth =
+      @prez_candidate.date_of_birth.change(year: (@election.year - 49))
     assert @prez_candidate.save
   end
 
   test "if not presidential candidate,  should be belong to constituency" do
-    #TODO(bubunyo) pull constituency model from petersowah and do test
+    # TODO(bubunyo) pull constituency model from petersowah and do test
   end
-
 end
