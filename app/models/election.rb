a1b41d2fbbc0a8349f3dd62aa700f12e3ee996c1
Yaw Boakye <wheresyaw@gmail.com>
2017-05-07 13:41:33 +0000

    Validate only if started_at and ended_at present

--- app/models/election.rb
+++ app/models/election.rb
@@ -7,17 +7,20 @@ class Election < ApplicationRecord
   validates :started_at, presence: true
   validates :ended_at,   presence: true
 
+
   private
 
     def started_at_cannot_be_later_than_ended_at
-      if started_at >= ended_at
-        errors.add(:started_at, "can't be later than ended at")
+      if started_at && ended_at
+        if started_at >= ended_at
+          errors.add(:started_at, "can't be later than ended at")
+        end
       end
     end
 
     def started_at_cannot_be_in_the_future
-      if started_at >= DateTime.now
-        errors.add(:started_at, "can't be in the future")
+      if started_at
+        errors.add(:started_at, "can't be in the future") unless started_at < DateTime.now
       end
     end
 end
