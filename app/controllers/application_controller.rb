90a2218818da31f35793034cf53db9594780e41c
Yaw Boakye <wheresyaw@gmail.com>
2017-05-07 13:40:08 +0000

    Moar JSON API compliance checks
    Make sure JSON payload has `data>attributes` and `type`. `data` and
    `type` are mandatory.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,29 @@ class ApplicationController < ActionController::API
   after_action :set_content_type, except: [:destroy]
   before_action :verify_accept_header
   before_action :verify_content_type
+  before_action :ensure_payload_conforms_to_jsonapi_format, only: [:create, :update]
 
   private
 
     def jsonapi_media_type
       "application/vnd.api+json".freeze
+    end
+
+    def prepare_error(error, status = :bad_request)
+      error.messages.each do |attr, messages|
+        # attr = :year
+        messages.each_with_index do |message, i|
+          code = Error::Codes.error_type_to_code(error.details[attr][i][:error])
+          errors.append(
+            status: status,
+            source: "data/attributes/#{attr}",
+            detail: "`#{attr.to_s}` #{message}",
+             title: "#{error.details[attr][i][:error].to_s} parameter",
+              code: code
+          )
+        end
+      end
+      errors
     end
 
     def set_content_type
@@ -26,7 +44,7 @@ class ApplicationController < ActionController::API
                        code: Error::Codes::UNSUPPORTED_MEDIA_TYPE,
                       links: {
                         about: "http://jsonapi.org/format/#content-negotiation"
-                      },
+                      }
                    )
         end
       end
@@ -48,4 +66,31 @@ class ApplicationController < ActionController::API
         end
       end
     end
+
+  def ensure_payload_conforms_to_jsonapi_format
+    unless params[:type]
+      errors.append(
+        source: :type,
+        status: :unprocessable_entity,
+         title: I18n.t("error.title.missing_resource_type"),
+        detail: I18n.t("error.detail.missing_resource_type"),
+          code: Error::Codes::MISSING_RESOURCE_TYPE
+      )
+    end
+    unless params[:data] && params[:data][:attributes]
+      errors.append(
+        source: :data,
+        status: :unprocessable_entity,
+        detail: I18n.t("error.detail.missing_data_object"),
+         title: I18n.t("error.title.missing_data_object"),
+          code: Error::Codes::MISSING_DATA
+      )
+    end
+
+    render status: :unprocessable_entity, json: errors unless errors.empty?
+  end
+
+  def errors
+    @errors ||= Error.new
+  end
 end
